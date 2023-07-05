72d243a542a44e7dd99a76b2d63aab054f2d732f
Rodrigo De Sousa Pereira <rodrigo25sousa@gmail.com>
2023-07-05 13:00:56 -0300

    component Sobre add

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 import './home.css';
 
 import Main from '../../components/Main';
+import Sobre from '../../components/Sobre';
 
 function Home(){
     return(
         <div className='app'>
             <Main></Main>
+            <Sobre></Sobre>
         </div>
     );
 }
