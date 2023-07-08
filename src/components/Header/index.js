903abad33e85c240fa2f542f1129f2df28dd365b
Rodrigo De Sousa Pereira <rodrigo25sousa@gmail.com>
2023-07-08 00:09:04 -0300

    fazer func de trocar Bars pelo X

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
 import './header.css';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 
-const mobileWidth = 800;
+const mobileWidth = 1000;
+
+let displayX = 'none';
+let displayBar = 'flex';
 
 function Header() {
+
+  function openMenu(){
+    
+  }
 
  return (
    <header>
@@ -11,7 +20,12 @@ function Header() {
 
         <div className='menu'>
 
-          {window.innerWidth <= mobileWidth && <button id='burguer'>Menu</button>} 
+          {window.innerWidth <= mobileWidth && 
+            <button id='burguer' onChange={openMenu}>
+              <FontAwesomeIcon style={{display: displayBar}} icon={faBars} />
+              <FontAwesomeIcon style={{display: displayX}} icon={faX} />
+            </button>
+          } 
 
           {window.innerWidth > mobileWidth && 
             <nav id='nav'>
