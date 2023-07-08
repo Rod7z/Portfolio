5eb946547b7bbdefa570dac191f3d46566db4965
Rodrigo De Sousa Pereira <rodrigo25sousa@gmail.com>
2023-07-07 21:11:13 -0300

    func btn

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,37 @@ import './header.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
+
+  const mobileWidth = 800;
+  
+  let burg = document.getElementById('burguer')
+  let nav = document.getElementById('nav')
+
+  function openMenu(){
+
+    if (window.innerWidth <= mobileWidth) {
+      
+    } else {
+
+    }
+  }
+
  return (
    <header>
         <div className='logo'>logo</div>
 
         <div className='menu'>
-            <ul>
-                <li><Link>Sobre</Link></li>
-                <li><Link>Habilidades</Link></li>
-                <li><Link>Projetos</Link></li>
-                <li><Link>Contato</Link></li>
-            </ul>
+            
+            <button id='burguer' style={{display: 'none'}} onClick={openMenu}>Menu</button>
+
+            <nav id='nav' style={{display: 'flex'}} onClick={openMenu}>
+              <ul>
+                  <li><Link>Sobre</Link></li>
+                  <li><Link>Habilidades</Link></li>
+                  <li><Link>Projetos</Link></li>
+                  <li><Link>Contato</Link></li>
+              </ul>
+            </nav>
         </div>
    </header>
  );
